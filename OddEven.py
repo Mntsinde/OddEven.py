4d494bf9ccbcbe057342485a2805091f4ca13e9f
Mntsinde <56126644+Mntsinde@users.noreply.github.com>
2025-09-07 22:34:58 +0100

    Add files via upload

---
+++ OddEven.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import math
+import re
+import random
+
+if __name__ == '--main--':
+    if n % 2 == 1:
+        print("Weird")
+    elif n % 2 == 0 and 2 <= n >= 5:
+        print("Not Weird")
+    elif n % 2 == 0 and 6 <= n >= 20:
+        print("Not Weird")
+    elif n % 2 == 0 and n> 20:
+        print("Not Weird")
